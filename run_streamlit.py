2204ccd31184e3139519e476ddc5ab7d4c32ad52
Shayne Longpre <shayne@Shaynes-MacBook-Pro.local>
2023-12-27 12:35:11 -0800

    wip

--- run_streamlit.py
+++ run_streamlit.py
@@ -104,8 +104,44 @@ def streamlit_app():
 
     RESOURCES = load_data()
 
-    st.markdown("""Select the preferred criteria for your datasets.""")
+    st.title("Open Foundation Model Cheatsheet")
+    st.caption("Resources and recommendations for best practices in developing and releasing open models.")
+    st.markdown("""This cheatsheet serves as a succinct guide, prepared *by* open foundation model developers *for* developers.
+        As AI foundation model development rapidly expands, welcoming new contributors, scientists, and
+        applications, we hope to help new community members become familiar with the latest resources, tools, and growing
+        body of research findings. The focus of this cheatsheet is not only to support building, but also to inculcate good
+        practices, awareness of limitations, and general responsible habits as community norms.""")
+    scope_limitations_text = """
+        There are many exceedingly popular tools to build, distribute and deploy foundation models. But there
+        are also many incredible resources that have gone less noticed, in the community's efforts to accelerate, deploy, and
+        monetize. We hope to bring wider attention to these core resources that support informed data selection, processing,
+        and understanding, precise and limitation-aware artifact documentation, resource-frugal model
+        training, advance awareness of the environmental impact from training, careful model evaluation
+        and claims, and lastly, responsible model release and deployment practices.
 
+        We've compiled strong resources, tools, and papers that have helped
+        guide our own intuitions around model development, and which we believe will be especially helpful to nascent
+        (and sometimes even experienced) developers in the field. However, this guide is certainly not comprehensive or
+        perfect—and here's what to consider when using it:
+        * They are scoped to 'open' model development, by which we mean the model weights will be publicly downloadable.
+        * Foundation model development is a rapidly evolving science. This cheatsheet is dated to December 2023, but
+        our repository is open for on-going public contributions: www.comingsoon.com.
+        * We've scoped our content modalities only to text, vision, and speech. We attempt to support multilingual
+        resources, but acknowledge these are only a starting point.
+        * A cheatsheet cannot be comprehensive. We rely heavily on survey papers and repositories to point out the
+        many other awesome works which deserve consideration, especially for developers who plan to dive deeper
+        into a topic.
+        * Lastly, we do not recommend all these resources for all circumstances, and have provided notes throughout
+        to guide this judgement. Instead we hope to bring awareness to good practices that many developers neglect
+        in the haste of development (eg careful data decontamination, documentation, and carefully specifying the
+        intended downstream uses).
+    """
+    with st.expander("Scope & Limitations"):
+        st.markdown(scope_limitations_text)
+    st.text("""Assembled by open model developers from AI2, EleutherAI, Google, Hugging Face, Masakhane, McGill, MIT, Princeton, Stanford CRFM, and UCSB.""")
+
+    # Clean up select menu.
+    # Add project header/instructions.
 
     ### SIDEBAR STARTS HERE
 
@@ -116,10 +152,11 @@ def streamlit_app():
         with st.form("data_selection"):
 
             section_multiselect = st.multiselect(
-                'Select the type of resources you are interested in',
+                'Resource Types:',
                 ["All"] + list(set(RESOURCES["Type"])),
                 ["All"])
 
+            st.text("Modality Types:")
             checkbox_text = st.checkbox("Text", value=True)
             checkbox_vision = st.checkbox("Vision")
             checkbox_speech = st.checkbox("Speech")
@@ -129,13 +166,13 @@ def streamlit_app():
             #     # options=
             #     value=datetime(2000, 1, 1))
 
-            date_format = 'MMM DD, YYYY'  # format output
+            date_format = 'MMM, YYYY'  # format output
             start_date = dt.date(year=2000,month=1,day=1) #-relativedelta(years=2)  #  I need some range in the past
             end_date = dt.datetime.now().date() #-relativedelta(years=2)
             max_days = end_date-start_date
             
             time_selection = st.slider(
-                'Select resources from this date onwards', 
+                'Start Date: ', 
                 min_value=start_date, 
                 value=start_date,
                 max_value=end_date)
@@ -144,10 +181,7 @@ def streamlit_app():
             # Every form must have a submit button.
             submitted = st.form_submit_button("Submit Selection")
 
-    #### SIDEBAR ENDS HERE
 
-
-    # st.write(len(INFO["data"]))
     if submitted:
         filtered_resources = filter_resources(
             RESOURCES, 
@@ -169,11 +203,8 @@ def streamlit_app():
             for i, row in section_resources.iterrows():
                 write_resource(row)
 
-# filter by modality
 # iterate on showing rows
 # show section introductions
-# Clean up select menu.
-# Add project header/instructions.
 
 
 if __name__ == "__main__":
